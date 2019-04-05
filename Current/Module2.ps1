94b79a674e1d372077e7791904e151b0baf1fc24
Meelis Nigols <meelis.nigols@telia.ee>
2019-04-05 08:56:39 +0300

    added Module 2

--- Current/Module2.ps1
+++ Current/Module2.ps1
@@ -18,14 +18,92 @@ throw "You're not supposed to run the entire script"
 
     # Lesson 1: Active Directory administration cmdlets
 
+Get-Command -Module ActiveDirectory | Measure-Object
+
+Get-Command -noun ADUser
+
+Get-ADUser -Identity Administrator
+Get-ADUser -Filter *
+
+Get-ADUser -Identity Administrator -Properties *
+Get-ADUser -Filter {Department -like 'IT'}
+
+$longAgo = (Get-Date).AddDays(-90)
+Get-ADUser -Filter {logonCount -ge 1 -and LastLogonDate -le $longAgo} |
+    Move-ADObject -TargetPath "ou=lost souls"
+
+
+$new = Get-ADUser "Mihkel Metsik"
+Get-ADUser -Filter {Department -like 'IT'} |
+    Set-ADUser -Manager $new
+
+Get-ADUser -filter * -Properties PasswordLastSet
+
+    # https://peterwawa.wordpress.com/2013/04/09/kasutajakontode-loomine-domeenis/
+
+$userParams = @{
+    Name = "Kati Kallike"
+    SamAccountName = "Kati"
+    GivenName = "Kati"
+    SurName = "Kallike"
+}
+New-ADUser @userParams
+
+$userParams | export-csv -UseCulture -Encoding Default -Path ./users.csv
+
+Import-Csv -UseCulture -Encoding Default -Path .\users.csv |
+    New-ADUser -AccountPassword (Get-Credential -Message 'Enter password for user').Password -Enabled $true
+
+Import-Csv .\modify.csv | ForEach-Object {
+    Set-ADUser -Identity $_.id -Add @{mail=$_.email}
+}
+
+Get-Command -Noun ADGroup*
+
+New-ADGroup -Name 'IT' -GroupScope Global
+Get-ADUser -Filter {Department -like 'IT'} |
+    Add-ADPrincipalGroupMembership -MemberOf 'IT'
+
+Get-ADGroup IT | Add-ADGroupMember -Members 'Mati'
+
+
+Get-Command -Noun ADComputer
+Get-ADComputer -Filter (PasswordLastSet -lt $longAgo) | Disable-ADAccount
+
+
+Get-Command -Noun ADOrganizationalUnit
+
+Get-ADOrganizationalUnit -Filter * |
+    Set-ADOrganizationalUnit -ProtectedFromAccidentalDeletion $false
 
 
     # Lesson 2: Network configuration cmdlets
 
+Get-Module Net*
+Get-Command -Module NetTCPIP
+Get-Command -Module NetAdapter
 
+Get-NetIPAddress -AddressFamily IPv4
+
+Resolve-DnsName -Name www.ee
+Resolve-DnsName -Name ttu.ee -Type mx
+
+Get-NetConnectionProfile
+
+Test-Connection -ComputerName www.ee
+Test-NetConnection -ComputerName www.ee -CommonTCPPort HTTP
+
+Get-NetTCPConnection -LocalPort 80
 
     # Lesson 3: Other server administration cmdlets
 
+Get-Command -Module GPO
 
+#Requires -Modules ServerManager
+Get-WindowsFeature
+Install-WindowsFeature -Name Telnet-Client -ComputerName myserver
+#Requires -RunAsAdministrator
+Get-WindowsOptionalFeature -Online
+Get-WindowsCapability -Online -Name *ssh*
 
-    # Lesson 4: Querying management information by using CIM and WMI
+Get-Command -Module Hyper-V
