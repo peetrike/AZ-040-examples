428bc25ebcd081443e3e40a90e4b3fb1151b19f1
Peter Wawa <meelisn@outlook.com>
2021-07-22 17:56:19 +0300

    Add Lesson 2 content

--- Modules/Module08.ps1
+++ Modules/Module08.ps1
@@ -39,15 +39,21 @@ pwsh -c (gc .\käsud.txt)
 # https://github.com/janikvonrotz/awesome-powershell#editors-and-ides
 
 # https://code.visualstudio.com
+Get-Command code
+code -r käsud.txt
+
 # https://docs.microsoft.com/powershell/scripting/windows-powershell/ise/how-to-write-and-run-scripts-in-the-windows-powershell-ise
+Get-Command ise
+ise käsud.txt
+
 # https://www.sapien.com/software/powershell_studio
 # https://visualstudio.microsoft.com/vs/community/
 
-# https://www.sublimetext.com/
-# https://atom.io/
 # https://github.com/neoclide/coc.nvim
 # https://emacs-lsp.github.io/lsp-mode/
 # https://plugins.jetbrains.com/plugin/10249
+# https://www.sublimetext.com/
+# https://atom.io/
 
 # https://code.labstack.com/powershell
 # https://poshgui.com/
@@ -64,6 +70,8 @@ pwsh -c (gc .\käsud.txt)
 
 Get-Module PowerShellGet -ListAvailable
 
+Get-Command -Noun Module, Script
+
 if ($PSVersionTable.PSVersion.Major -lt 6) {
     if (-not ([Net.ServicePointManager]::SecurityProtocol -band [Net.SecurityProtocolType]::Tls12)) {
         [Net.ServicePointManager]::SecurityProtocol =
@@ -72,7 +80,7 @@ if ($PSVersionTable.PSVersion.Major -lt 6) {
 }
 
 Find-Script Send-PasswordNotification -Repository PSGallery
-find-module UserProfile -Repository PSGallery
+Find-Module UserProfile -Repository PSGallery
 Find-Module -Command get-user -Repository PSGallery
 
 Get-InstalledScript
@@ -94,10 +102,13 @@ pwsh -f käsud.ps1
 powershell.exe -command .\käsud.ps1
 .\käsud.ps1
 .\käsud
+käsud
 
 Get-Help Command_Precedence -ShowWindow
 
 Get-Help Run_With_PowerShell -ShowWindow
+
+Get-Help Unblock-File -ShowWindow
 
 #endregion
 
@@ -144,11 +155,178 @@ Get-Command -Noun FileCatalog
 
 #region Lesson 2: Scripting constructs
 
-Get-Help about_foreach -ShowWindow
+#region Understanding foreach loops
 
-foreach ($i in 1..1KB) {
+Get-Help about_Foreach -Category HelpFile -ShowWindow
+
+foreach ($i in 1..10) {
     "number on $i"
 }
+
+foreach ($file in Get-ChildItem) {
+    $file.Name
+}
+
+Get-Help ForEach-Object -ShowWindow
+Get-Help Pipelines -Category HelpFile -ShowWindow
+
+#endregion
+
+#region Understanding the If construct
+
+Get-Help about_If -ShowWindow
+
+if ($true) { 'on tõene' }
+elseif ($false) { 'on väär' }
+else { 'on midagi muud' }
+
+if (Get-ChildItem -File) { 'on faile ' }
+else { new-item uus.txt }
+
+#endregion
+
+#region Understanding the Switch construct
+
+Get-Help Switch -Category HelpFile -ShowWindow
+
+$choice = Get-Random -Maximum 4
+switch ($choice) {
+    1 { 'You selected menu item 1' }
+    2 { 'You selected menu item 2' }
+    3 { 'You selected menu item 3' }
+    default { Write-Warning -Message 'You did not select a valid option' }
+}
+
+$choice = Read-Host -Prompt 'kas Sa tahad jätkata? [j/e]'
+switch -Wildcard ($choice) {
+    '[jy]*' { 'olgu' }
+    'e*' { 'kahju' }
+    Default { 'proovi uuesti' }
+}
+
+$ip = Get-NetIPAddress -InterfaceAlias wi-fi -AddressFamily IPv4
+Switch -WildCard ($ip.IPAddress) {
+    '10.*' { 'This computer is on the internal network' }
+    '10.1.*' { 'This computer is in London' }
+    '10.2.*' { 'This computer is in Vancouver' }
+    default { Write-Warning -Message 'This computer is not on the internal network' }
+}
+
+switch -Regex (Get-Random -Maximum 11) {
+    '(1|7)$' { $_ }
+    '2|8' { $_ * 3 }
+    '[1-5]' { 'lower part' }
+    { $_ % 2 } { 'Odd number' }
+    '^\d$' { 'lower than 10' }
+    default { Write-Warning -Message 'Missed it' }
+}
+
+$computer = 'LON-CL1'
+$role = 'unknown role'
+$location = 'unknown location'
+Switch -WildCard ($computer) {
+    '*-CL*' { $role = 'client' }
+    '*-SRV*' { $role = 'server' }
+    '*-DC*' { $role = 'domain controller' }
+    { $_ -like 'L*' } {
+        $location = 'London'
+    }
+    'VAN-*' { $location = 'Vancouver' }
+    Default { "$computer is not a valid name" }
+}
+"$computer is a $role in $location"
+
+$numbers = 1..4 | ForEach-Object { Get-Random -Maximum 5 }
+switch ($numbers) {
+    3 { Write-Host -ForegroundColor Cyan '3 this time' }
+    { $_ % 2 } { 'Odd number: {0}' -f $_ }
+    5 { 'Got it!' }
+    Default { Write-Warning -Message 'Missed it' }
+}
+
+#endregion
+
+#region Understanding the For construct
+
+Get-Help about_For -ShowWindow
+
+for ($i = 1; $i -le 10; $i++) { "Creating User $i" }
+
+for (
+    $i = 1
+    $i -le 10
+    $i += 2
+) {
+    "Creating User $i"
+}
+
+#endregion
+
+
+#region Understanding other loop constructs
+
+Get-Help about_Do -ShowWindow
+Get-Help about_While -ShowWindow
+
+Do {
+    'Script block to process'
+    $answer = Read-Host -Prompt 'Go or Stop'
+} While ($answer -eq 'go')
+
+Do {
+    'Script block to process'
+    $answer = Read-Host -Prompt 'Go or Stop'
+} Until ($answer -eq 'stop')
+
+Remove-Variable -Name i
+while (++$i -le 10) {
+    'number on {0}' -f $i
+}
+$i
+
+while ($true) {
+    'Processing...'
+    Start-Sleep -Seconds 2
+}
+
+#endregion
+
+#region Understanding Break and Continue
+
+Get-Help about_Break -ShowWindow
+Get-Help about_Continue -ShowWindow
+
+$users = Get-ADUser -Filter * -ResultSetSize 20
+
+ForEach ($user in $users) {
+    If ($user.Name -eq 'Administrator') { continue }
+    'Modify user object'
+}
+
+$max = 3
+ForEach ($user in $users) {
+    $number++
+    "Modify User object $number"
+    If ($number -ge $max) { break }
+}
+'After loop'
+
+$ip = '10.1.2.3', '300.3.986.4', '10.3.4.5', '10.2.3.7', '13.9.4.5'
+switch -Regex ($ip) {
+    '^10\.1' {
+        "$_ is in London"
+        continue
+    }
+    "^10`.2`." {
+        'This computer is in Vancouver'
+        break
+    }
+    "10`." { '{0} is on the internal network' -f $_ }
+    { -not ($_ -as [ipaddress]) } { Write-Warning -Message ('not a valid IP address: {0}' -f $_) }
+    default { '{0} is not on the internal network' -f $_ }
+}
+
+#endregion
 
 #endregion
 
