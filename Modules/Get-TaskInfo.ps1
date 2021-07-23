594eb6a46540d33f09162366a5b816196bf7921d
Peter Wawa <meelisn@outlook.com>
2021-07-23 20:19:33 +0300

    Put Get-TaskInfo into separate file

---
+++ Modules/Get-TaskInfo.ps1
@@ -0,0 +1,55 @@
+﻿function Get-TaskInfo {
+    [CmdletBinding()]
+    param(
+            [ValidateNotNullOrEmpty()]
+            [string]
+        $TaskName = '*',
+            [string]
+        $TaskPath
+    )
+
+    function Get-TaskFolder {
+        [CmdletBinding()]
+        param (
+            $Folder
+        )
+        $Folder
+        foreach ($subFolder in $Folder.GetFolders(0)) {
+            Get-TaskFolder -Folder $subFolder
+        }
+    }
+
+    function Get-Task {
+        param (
+                [string]
+            $Name = '*',
+            $Folder
+        )
+
+        if ($Name -and $Name -notmatch '\*') {
+            try {
+                $Folder.GetTask($Name)
+            } catch {
+                Write-Verbose -Message ('No tasks in folder {0}' -f $Folder.Path)
+            }
+        } else {
+                # include hidden tasks
+            $Folder.GetTasks(1) | Where-Object { $_.Name -like $Name }
+        }
+    }
+
+    $Scheduler = New-Object -ComObject 'Schedule.Service'
+    $Scheduler.Connect()
+    $RootFolder = $Scheduler.GetFolder('\')
+
+    if ($TaskName -match '\\') {
+        $RootFolder.GetTask($TaskName)
+    } elseif ($TaskPath) {
+        $currentFolder = $RootFolder.GetFolder($TaskPath)
+        Get-Task -Name $TaskName -Folder $currentFolder
+    } else {
+        foreach ($folder in Get-TaskFolder -Folder $RootFolder) {
+            Get-Task -Name $TaskName -Folder $folder
+        }
+    }
+}
--- Modules/Module03.ps1
+++ Modules/Module03.ps1
@@ -245,62 +245,8 @@ Measure-Command {
     Get-ScheduledTask | where TaskName -eq 'katse'
 }
 
-function Get-TaskInfo {
-    [CmdletBinding()]
-    param(
-            [ValidateNotNullOrEmpty()]
-            [string]
-        $TaskName = '*',
-            [string]
-        $TaskPath
-    )
-
-    function Get-TaskFolder {
-        [CmdletBinding()]
-        param (
-            $Folder
-        )
-        $Folder
-        foreach ($subFolder in $Folder.GetFolders(0)) {
-            Get-TaskFolder -Folder $subFolder
-        }
-    }
-
-    function Get-Task {
-        param (
-                [string]
-            $Name = '*',
-            $Folder
-        )
-
-        if ($Name -and $Name -notmatch '\*') {
-            try {
-                $Folder.GetTask($Name)
-            } catch {
-                Write-Verbose -Message ('No tasks in folder {0}' -f $Folder.Path)
-            }
-        } else {
-                # include hidden tasks
-            $Folder.GetTasks(1) | where { $_.Name -like $Name }
-        }
-    }
-
-    $Scheduler = New-Object -ComObject 'Schedule.Service'
-    $Scheduler.Connect()
-    $RootFolder = $Scheduler.GetFolder('\')
-
-    if ($TaskName -match '\\') {
-        $RootFolder.GetTask($TaskName)
-    } elseif ($TaskPath) {
-        $currentFolder = $RootFolder.GetFolder($TaskPath)
-        Get-Task -Name $TaskName -Folder $currentFolder
-    } else {
-        foreach ($folder in Get-TaskFolder -Folder $RootFolder) {
-            # include hidden tasks
-        Get-Task -Name $TaskName -Folder $folder
-        }
-    }
-}
+# import function
+. .\Get-TaskInfo.ps1
 
 Measure-Command {
     Get-TaskInfo -TaskName meelis\katse
