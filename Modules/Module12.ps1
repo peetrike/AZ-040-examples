5217873e01b522252e35031a528a642325ee352d
Peter Wawa <meelisn@outlook.com>
2021-07-27 11:42:32 +0300

    Add Module 12 content

--- Modules/Module12.ps1
+++ Modules/Module12.ps1
@@ -17,13 +17,29 @@
 
 #region Lesson 1: Creating profile scripts
 
-get-help about_profiles -ShowWindow
+#region What is a profile script?
+
+Get-Help about_Profiles -ShowWindow
+
+#endregion
+
+#region Profile script locations
 
 $profile
 
 $profile | Get-Member -MemberType NoteProperty
 $profile.CurrentUserCurrentHost
 $profile.AllUsersAllHosts
+
+#endregion
+
+#region Profile script security concerns
+
+# https://docs.microsoft.com/powershell/module/microsoft.powershell.core/about/about_profiles#profiles-and-execution-policy
+
+Get-Help about_Execution_Policies -ShowWindow
+
+#endregion
 
 #endregion
 
@@ -51,23 +67,78 @@ Invoke-Item credential.xml
 
     # PowerShell 7+
 Find-Module Microsoft.PowerShell.SecretManagement
+
 #endregion
 
 #region Array operators
 
 # https://docs.microsoft.com/powershell/module/microsoft.powershell.core/about/about_comparison_operators#containment-operators
 
+1 -in 1, 2, 3, 4
+1 -in 2, 3, 4, 5
+
+1, 2, 3, 4 -contains 1
+2, 3, 4, 5 -contains 1
+
+
+    # this doesn't work
+Get-Process notepad | Stop-Process
+Start-Process notepad
+$p = Get-Process notepad
+$p -in (Get-Process notepad)
+(Get-Process) -contains $p
+
 #endregion
 
 #region Regular expressions
 
-Get-Help regular -ShowWindow
+Get-Help Regular -ShowWindow
+
+$Matches
+'mis kinni ei jää, saab kinni löödud' -match 'kinni'
+$Matches
+'mis kinni ei jää, saab kinni löödud' -match '.*kinni.*'
+$Matches
+
+'Windows PowerShell', 'Command Prompt' -match 'Power.*'
+$Matches
+
+'Windows PowerShell', 'Command Prompt' | Select-String -Pattern 'Power.*'
+Select-String -Pattern 'Module 12' -Path module*.ps1 -List
+
+    # https://docs.microsoft.com/dotnet/api/system.text.regularexpressions.regex
+$expression = [regex] 'kinni'
+$expression.Match('mis kinni ei jää, saab kinni löödud')
+$expression.Matches('mis kinni ei jää, saab kinni löödud')
+
+$string = 'The last logged on user was CONTOSO\jsmith'
+
+$string -match '.+was (.+)'
+$Matches
+$Matches.1
+
+$string -match 'was (?<domain>\w+)\\(?<user>\w+)'
+$Matches
+$Matches.user
 
 #endregion
 
 #region The format operator
 
 # https://docs.microsoft.com/powershell/module/microsoft.powershell.core/about/about_operators#format-operator--f
+# https://docs.microsoft.com/dotnet/standard/base-types/formatting-types
+
+'Money: {0:c}' -f 1.24
+'Percent {0:p}' -f 0.36
+'Hex: {0:x}' -f 349
+
+'Custom: {0:00.00}' -f 3.2
+'Custom: {0:##.00}' -f 3.2
+'Custom: {0,10:f2}' -f 1.0782345
+
+'Sortable date: {0:s}' -f [datetime]::Now
+'Sortable date: ' + [datetime]::Now.ToString('s')
+'ISO 8601 format: {0:o}' -f [datetime]::Now
 
 #endregion
 
@@ -77,6 +148,7 @@ get-command ping
 get-command sc.exe
 
 Get-Help about_Parsing -ShowWindow
+# https://docs.microsoft.com/powershell/module/microsoft.powershell.core/about/about_special_characters#stop-parsing-token---
 
 bcdedit.exe --% -default {current}
 
@@ -85,8 +157,21 @@ bcdedit.exe --% -default {current}
 #region Working with NTFS permissions
 
 Get-Command -Noun Acl
+$acl = get-acl -Path module12.ps1
+$acl
+$acl.Access
+
+Get-Help Set-Acl -ShowWindow
 
 Find-Module NTFSSecurity
+
+Get-NTFSAccess -Path module12.ps1
+Get-NTFSAudit -Path module12.ps1
+
+Get-Help Add-NTFSAccess -ShowWindow
+Get-Help Set-NTFSOwner -ShowWindow
+Get-Help Set-NTFSInheritance -ShowWindow
+Get-Help Remove-NTFSAccess -ShowWindow
 
 #endregion
 
@@ -108,9 +193,18 @@ Get-Help Start-Transcript -ShowWindow
 Get-Help about_History -ShowWindow
 Get-Command -Noun History
 
+#endregion
+
+#region Extra: Logging from scripts
 
 Get-Command -noun WinEvent
 # https://peterwawa.wordpress.com/2015/02/26/powershell-ja-sndmuste-logid/
+
+Find-Module -Command Write-Log -Repository PSGallery
+Find-Module PSFramework -Repository PSGallery
+
+# https://psframework.org/documentation/documents/psframework/logging.html
+Get-Help Write-PSFMessage -ShowWindow
 
 #endregion
 
