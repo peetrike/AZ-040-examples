166af3a3747d8e5114a5753529ab694cd2cd1ddd
Peter Wawa <meelisn@outlook.com>
2021-11-26 23:14:40 +0200

    Update Module 4 samples

--- Modules/Module04.ps1
+++ Modules/Module04.ps1
@@ -19,9 +19,15 @@
 
 #region Pipeline parameter binding
 
+Get-Help about_Parameters -ShowWindow
 # https://docs.microsoft.com/powershell/module/microsoft.powershell.core/about/about_parameters#accepts-pipeline-input
 
+(Get-Command Set-ADUser).Parameters.Identity.ParameterType.FullName
+Get-Help Set-ADUser -Parameter Identity
+
 Get-ADUser -Filter { Name -like 'Adam*' } | Set-ADUser -City 'Tallinn'
+
+Get-Help Restart-Service -Parameter InputObject
 
 Get-Service B* | Restart-Service -WhatIf
 
@@ -29,10 +35,22 @@ Get-Service B* | Restart-Service -WhatIf
 
 #region Identifying ByValue parameters
 
+(Get-Help Get-ADUser).returnValues.returnValue.type.name
+(Get-Help Set-ADUser).parameters.parameter |
+    Where-Object pipelineInput -like 'true*' |
+    Select-Object name, pipelineInput, type
 Get-Help Set-ADUser -Parameter Identity
+
+$type = (Get-Command Get-Service).OutputType.Type
+(Get-Command Restart-Service).Parameters.Values | Where-Object {
+    $_.Attributes.ValueFromPipeLine -and
+    $type.Name -like $_.ParameterType.Name.replace('[]', '')
+}
+Get-Help Restart-Service -Parameter InputObject
+
+# https://github.com/indented-automation/Indented.Profile/blob/master/Indented.Profile/public/Get-ParameterInfo.ps1
+Get-ParameterInfo -Name Sort-Object | Where-Object PipeLine -NotLike 'None'
 Get-Help Sort-Object -Parameter InputObject
-Get-Help Set-Service -Parameter InputObject
-Get-Help Start-Service -Parameter InputObject
 
 Get-Command -ParameterName InputObject | Measure-Object
 
@@ -43,26 +61,28 @@ Get-Command -ParameterName InputObject | Measure-Object
     # you can do this
 $Services = Get-Service p*
 Start-Service -InputObject $Services -WhatIf
-    # but this is more convinient
+    # but this is more convenient
 Get-Service p* | Start-Service -WhatIf
 Get-Service bits | Set-Service -StartupType Automatic -WhatIf
 
 Get-Help Start-Service -Parameter Name
 'bits', 'winrm' | Start-Service
 
-    # PowerShell 7 removed -ComputerName from Get/Set-Service
 Get-Help Get-Service -Online
 
     # Exporting to CSV retains object type
 Get-Service p* | Export-Csv -Path Services.csv
-Import-Csv Services.csv | Get-Service | Start-Service -WhatIf
+    #Requires -Version 7
+Get-Service p* | Export-Csv -Path Services.csv -IncludeTypeInformation
+Import-Csv Services.csv | Get-Member
+Import-Csv Services.csv | Start-Service -WhatIf
 
 #endregion
 
 #region Passing pipeline data ByPropertyName
 
     # this doesn't work
-Get-ADComputer $env:COMPUTERNAME | Test-Connection
+Get-ADComputer $env:COMPUTERNAME | Test-Connection -Count 1
 
 Get-ADComputer $env:COMPUTERNAME | Get-Member
 Get-Help Test-Connection -Parameter *
@@ -78,6 +98,8 @@ Get-ADComputer -Filter * | Test-Connection -ComputerName { $_.DnsHostName } -Cou
 #endregion
 
 #region Identifying ByPropertyName parameters
+
+Get-ParameterInfo -Name New-ADUser
 
 Get-Help Stop-Process -Parameter *
 Get-Help Stop-Service -Parameter *
@@ -97,7 +119,7 @@ Get-ChildItem | Select-Object -First 1 | Stop-Service
 Get-Help Stop-Service -Parameter Name
 Get-ChildItem | Get-Member Name
 
-start-process notepad
+Start-Process notepad
     # Wrong ParameterSet
 Get-Process -Name notepad | Stop-Process -Name notepad
 
@@ -110,8 +132,8 @@ Get-Process -Name notepad | Stop-Process -Name notepad
 $kasutajad = Get-ADUser -Filter { City -like 'London' }
 Add-ADGroupMember 'London Users' -Members $kasutajad
     # or
-Add-ADGroupMember 'London Users' -Members (Get-ADUser -filter { city -like 'London' })
-Get-ADUser -filter { city -like 'London' } |
+Add-ADGroupMember 'London Users' -Members (Get-ADUser -Filter { City -like 'London' })
+Get-ADUser -Filter { city -like 'London' } |
     Add-ADPrincipalGroupMembership -MemberOf 'London Users'
 
     # same users to several groups
