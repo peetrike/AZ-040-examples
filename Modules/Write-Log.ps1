7894fd274bf961209bd1b1cbbb90e66d59b65bbc
Peter Wawa <meelisn@outlook.com>
2021-12-04 08:35:12 +0200

    Refactor function
    - make it accept pipeline input
    - create array of actionpreferences that indicate the need of logging
    - move -AddEmptyLine to end block

--- Modules/Write-Log.ps1
+++ Modules/Write-Log.ps1
@@ -65,7 +65,7 @@ Function Write-Log {
                 Join-Path $PWD WriteLog.txt
             }
         ),
-        [ValidateSet('Error', 'Warning', 'Info', 'Verbose', 'Debug', 'Log', 'Empty')]
+            [ValidateSet('Error', 'Warning', 'Info', 'Verbose', 'Debug', 'Log', 'Empty')]
             [string]
         $Type = 'Info',
             [bool]
@@ -76,70 +76,83 @@ Function Write-Log {
         $AddEmptyLine
     )
 
-    $TimeStamp = Get-Date -Format G
-    $Time = Get-Date -Format T
-    if ($NoDate) {
-        $screenPattern = '{1}'
-        $msgPattern = '{0,-9} - {2}'
-    } else {
-        $screenPattern = '{0} - {1}'
-        $msgPattern = '{0,-9} - {1} - {2}'
+    begin {
+        if ($NoDate) {
+            $screenPattern = '{1}'
+            $msgPattern = '{0,-9} - {2}'
+        } else {
+            $screenPattern = '{0} - {1}'
+            $msgPattern = '{0,-9} - {1} - {2}'
+        }
+        $ActionPreferenceList = @(
+            [Management.Automation.ActionPreference]::Stop
+            [Management.Automation.ActionPreference]::Continue
+            [Management.Automation.ActionPreference]::Inquire
+            [Management.Automation.ActionPreference]::Suspend
+        )
     }
 
-    switch ($Type) {
-        'Error' {
-            $prefix = '[ERROR]'
-            Write-Error ($screenPattern -f $Time, $Message)
-            if ($WriteLog -and ($ErrorActionPreference -in 1, 2, 3, 5)) {
-                Add-Content -Path $Path -Value ($msgPattern -f $prefix, $TimeStamp, $Message)
-            }
-        }
-        'Warning' {
-            $prefix = '[WARNING]'
-            Write-Warning -Message ($screenPattern -f $Time, $Message)
-            if ($WriteLog -and ($WarningPreference -in 1, 2, 3, 5 )) {
-                Add-Content -Path $Path -Value ($msgPattern -f $prefix, $TimeStamp, $Message)
-            }
-        }
-        'Info' {
-            $prefix = '[INFO]'
-            if (Get-Command Write-Information -ErrorAction SilentlyContinue) {
-                Write-Information -MessageData ($screenPattern -f $Time, $Message) -InformationAction Continue -Tags 'Log'
-            } else {
-                Write-Host ($screenPattern -f $Time, $Message)
-            }
-            $Logging = $true
-            if ($InformationPreference) {
-                $Logging = $InformationPreference -in 1, 2, 3, 5
-            }
-            if ($WriteLog -and $Logging) {
-                Add-Content -Path $Path -Value ($msgPattern -f $prefix, $TimeStamp, $Message)
-            }
-        }
-        'Verbose' {
-            $prefix = '[VERBOSE]'
-            Write-Verbose -Message ($screenPattern -f $Time, $Message)
-            if ($WriteLog -and ($VerbosePreference -in 1, 2, 3, 5 )) {
-                Add-Content -Path $Path -Value ($msgPattern -f $prefix, $TimeStamp, $Message)
-            }
-        }
-        'Debug' {
-            $prefix = '[DEBUG]'
-            Write-Debug -Message ($screenPattern -f $Time, $Message)
-            if ($WriteLog -and ($DebugPreference -in 1, 2, 3, 5 )) {
-                Add-Content -Path $Path -Value ($msgPattern -f $prefix, $TimeStamp, $Message)
-            }
-        }
-        'Log' {
-            $prefix = '[LOG]'
-            if ($WriteLog) {
-                Add-Content -Path $Path -Value ($msgPattern -f $prefix, $TimeStamp, $Message)
-            }
-        }
-    }
+    process {
+        $TimeStamp = [datetime]::Now.ToString('G')
+        $Time = [datetime]::Now.ToString('T')
 
-    if ($AddEmptyLine -or ($Type -like 'Empty')) {
-        Add-Content -Value '' -Path $Path -WhatIf:$false -Confirm:$false
+        switch ($Type) {
+            'Error' {
+                $prefix = '[ERROR]'
+                Write-Error ($screenPattern -f $Time, $Message)
+                if ($WriteLog -and ($ErrorActionPreference -in $ActionPreferenceList)) {
+                    Add-Content -Path $Path -Value ($msgPattern -f $prefix, $TimeStamp, $Message)
+                }
+            }
+            'Warning' {
+                $prefix = '[WARNING]'
+                Write-Warning -Message ($screenPattern -f $Time, $Message)
+                if ($WriteLog -and ($WarningPreference -in $ActionPreferenceList)) {
+                    Add-Content -Path $Path -Value ($msgPattern -f $prefix, $TimeStamp, $Message)
+                }
+            }
+            'Info' {
+                $prefix = '[INFO]'
+                if (Get-Command Write-Information -ErrorAction SilentlyContinue) {
+                    Write-Information -MessageData ($screenPattern -f $Time, $Message) -InformationAction Continue -Tags 'Log'
+                } else {
+                    Write-Host ($screenPattern -f $Time, $Message)
+                }
+                $Logging = $true
+                if ($InformationPreference) {
+                    $Logging = $InformationPreference -in $ActionPreferenceList
+                }
+                if ($WriteLog -and $Logging) {
+                    Add-Content -Path $Path -Value ($msgPattern -f $prefix, $TimeStamp, $Message)
+                }
+            }
+            'Verbose' {
+                $prefix = '[VERBOSE]'
+                Write-Verbose -Message ($screenPattern -f $Time, $Message)
+                if ($WriteLog -and ($VerbosePreference -in $ActionPreferenceList)) {
+                    Add-Content -Path $Path -Value ($msgPattern -f $prefix, $TimeStamp, $Message)
+                }
+            }
+            'Debug' {
+                $prefix = '[DEBUG]'
+                Write-Debug -Message ($screenPattern -f $Time, $Message)
+                if ($WriteLog -and ($DebugPreference -in $ActionPreferenceList)) {
+                    Add-Content -Path $Path -Value ($msgPattern -f $prefix, $TimeStamp, $Message)
+                }
+            }
+            'Log' {
+                $prefix = '[LOG]'
+                if ($WriteLog) {
+                    Add-Content -Path $Path -Value ($msgPattern -f $prefix, $TimeStamp, $Message)
+                }
+            }
+        }
+
+    }
+    end {
+        if ($AddEmptyLine -or ($Type -like 'Empty')) {
+            Add-Content -Value '' -Path $Path -WhatIf:$false -Confirm:$false
+        }
     }
 }
 
