3def07c3b1ff747a16ac787c1dee2266e3b9d926
Peter Wawa <meelisn@outlook.com>
2021-12-11 10:38:31 +0200

    Add changes from events05 script

--- Modules/events06.ps1
+++ Modules/events06.ps1
@@ -1,23 +1,27 @@
 ﻿<#
     .SYNOPSIS
-        see skript otsib sündmuste logist asju
+        otsib sündmuste logist asju
     .DESCRIPTION
-        pikem kirjeldus sellest, mida skript teeb ...
+        See skript otsib Application logist sündmusi, mille EventId on 15
     .PARAMETER ComputerName
         See parameeter ütleb et kust arvutist sündmused võtta.
     .EXAMPLE
-        .\events06.ps1 -aeg (get-date).adddays(-3)
+        .\events06.ps1 -Aeg (Get-Date).AddDays(-3)
 
         Leiab 3 päeva vanad sündmused
     .LINK
         Get-WinEvent
+    .LINK
+        http://www.ee
+    .NOTES
+        Author: Meelis Nigols
 #>
 
 [CmdletBinding()]
 param (
         [datetime]
         # aeg, millest alates sündmusi otsida
-    $aeg = [datetime]::Now.AddDays(-1),
+    $Aeg = [datetime]::Now.AddDays(-1),
         [ValidatePattern('Lon-*')]
         [Alias('CN')]
         [string]
