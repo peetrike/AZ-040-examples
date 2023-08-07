6598a29873f6d13078b5c05a520d68a3e6ec18ed
Peter Wawa <meelisn@outlook.com>
2023-08-07 13:42:34 +0300

    Move Lab 3B before Lesson 6

--- Modules/Module03.ps1
+++ Modules/Module03.ps1
@@ -394,11 +394,13 @@ Measure-Command {
 
 #endregion
 
+
 #region Lab 3A
 
 # https://github.com/MicrosoftLearning/AZ-040T00-Automating-Administration-with-PowerShell/blob/master/Instructions/Labs/LAB_03A_Working_with_Windows_PowerShell_Pipeline.md
 
 #endregion
+
 
 #region Lesson 4: Enumerating objects in the pipeline
 
@@ -592,6 +594,13 @@ Find-Module PSWriteOffice -Repository PSGallery
 #endregion
 
 
+#region Lab 3B
+
+# https://github.com/MicrosoftLearning/AZ-040T00-Automating-Administration-with-PowerShell/blob/master/Instructions/Labs/LAB_03B_Working_with_Windows_PowerShell_Pipeline.md
+
+#endregion
+
+
 #region Lesson 6: Passing pipeline data
 
 #region Pipeline parameter binding
@@ -750,11 +759,5 @@ Get-ADUser -Id Tia -Properties $Property |
 Get-ADUser -Id Tia | Get-ADPrincipalGroupMembership
 
 #endregion
-
-#endregion
-
-#region Lab 3B
-
-# https://github.com/MicrosoftLearning/AZ-040T00-Automating-Administration-with-PowerShell/blob/master/Instructions/Labs/LAB_03B_Working_with_Windows_PowerShell_Pipeline.md
 
 #endregion
